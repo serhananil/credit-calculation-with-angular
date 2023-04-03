8b929b37738a85d132df5c5eec51ebd119b47426
Serhan Anıl Kılıç <78823809+serhananil@users.noreply.github.com>
2023-04-03 16:19:54 +0300

    ++

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardHeaderComponent } from './card-header/card-header.component';
+import { CardBodyComponent } from './card-body/card-body.component';
+import { CardFooterComponent } from './card-footer/card-footer.component';
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardHeaderComponent,
+    CardBodyComponent,
+    CardFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
